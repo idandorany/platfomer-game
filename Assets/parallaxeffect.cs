ad98adbc10e0bc8b4c1548f5f3d1bc718cd42bfe
idandorany <idan98765432123@gmail.com>
2024-08-05 01:23:08 +0300

    1

---
+++ Assets/parallaxeffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxeffect : MonoBehaviour
+{ 
+    public Camera cam;
+    public Transform followTarget;
+
+    Vector2 startPosition;
+
+    float startingZ;
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class playercontroller : MonoBehaviour
 {
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+
+    public bool IsMoving {  get; private set; }
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +30,16 @@ public class playercontroller : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed ,rb.velocity.y );
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput !=Vector2.zero;
     }
 }
